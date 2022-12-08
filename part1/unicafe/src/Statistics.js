e30d4ca716563b3bc54c10343edd1489563c72d8
justgast <justgast@gmail.com>
2022-12-08 17:06:25 +0300

    Part 1: Exercise 1.7 ready;

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,10 +1,27 @@
-const Statistics = ({good, neutral, bad}) => (
-  <>
-    <h2>Statistics</h2>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </>
-);
+const Statistics = ({good, neutral, bad}) => {
+  let noReviews = "no reviews yet";
+
+  const countAverage = () => ((1 * good) + (-1 * bad)) / all
+  const countPositive = () => good / all * 100
+
+  const all = good + neutral + bad;
+  const average = all ? countAverage() : noReviews;
+  const positive = all ? countPositive() : noReviews;
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+      <br/>
+
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </>
+  );
+};
 
 export default Statistics;
