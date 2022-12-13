303248ecc277a0d0ec99fdd77ec2abf419feb514
justgast <justgast@gmail.com>
2022-12-13 14:43:42 +0300

    Part 2: Exercises 2.19-2.20 ready;

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,7 @@ import Filter from "./components/Filter";
 import AddForm from "./components/AddForm";
 import NumbersList from "./components/NumbersList";
 import personsService from './services/persons';
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -12,6 +13,9 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('');
 
   const [filter, setFilter] = useState('');
+
+  const emptyMessage = {message: '', type: ''};
+  const [message, setMessage] = useState(emptyMessage);
 
   useEffect(() => {
     personsService.getAll()
@@ -22,6 +26,11 @@ const App = () => {
   const personsShown = persons.filter(
     person => person.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const showNotification = (message, type) => {
+    setMessage({message, type})
+    setTimeout(() => setMessage(emptyMessage), 5000);
+  }
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -41,7 +50,9 @@ const App = () => {
         setPersons(persons.concat(newPerson));
         setNewName('');
         setNewNumber('');
+        showNotification(`Person ${newPerson.name} was created`, 'success');
       })
+      .catch(error => console.log(error.message));
   };
 
   const deletePerson = id => () => {
@@ -54,6 +65,11 @@ const App = () => {
       .deletePerson(id)
       .then(() => {
         setPersons(persons.filter(person => person.id !== id));
+        showNotification(`Person ${deletedPerson.name} was deleted`, 'success');
+      })
+      .catch(() => {
+        setPersons(persons.filter(person => person.id !== id));
+        showNotification(`Person ${deletedPerson.name} was already deleted`, 'danger');
       });
   }
 
@@ -66,6 +82,15 @@ const App = () => {
       .update(person.id, {...person, number: newNumber})
       .then(newPersonData => {
         setPersons(persons.map(person => person.id === newPersonData.id ? newPersonData : person))
+        showNotification(`Person ${newPersonData.name} was updated`, 'success');
+      })
+      .catch(error => {
+        if (error.response.status === 404) {
+          setPersons(persons.filter(currentPerson => currentPerson.id !== person.id));
+          showNotification(`Person ${person.name} has already been removed from server`, 'danger');
+        } else {
+          showNotification(error.message, 'danger');
+        }
       });
   }
 
@@ -73,6 +98,8 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
       <Filter filter={filter} onChange={(e) => setFilter(e.target.value)}/>
+
+      {message && <Notification message={message.message} type={message.type}/>}
 
       <h2>Add new</h2>
       <AddForm
