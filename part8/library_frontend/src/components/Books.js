fc5060b40dff30d2c20dbe2d42c462775fbbd3d3
justgast <justgast@gmail.com>
2023-02-07 16:40:11 +0300

    Part 8: Exercise 8.19 ready;

--- part8/library_frontend/src/components/Books.js
+++ part8/library_frontend/src/components/Books.js
@@ -1,18 +1,25 @@
 import {useQuery} from '@apollo/client';
 import {ALL_BOOKS} from '../queries';
+import {useState} from 'react';
 
 const Books = (props) => {
-  const booksQueryResult = useQuery(ALL_BOOKS)
+  const booksQueryResult = useQuery(ALL_BOOKS);
+  const [genreFilter, setGenreFilter] = useState(null);
 
   if (!props.show) {
-    return null
+    return null;
   }
 
   if (booksQueryResult.loading) {
-    return <>Loading...</>
+    return <>Loading...</>;
   }
 
-  const {allBooks: books} = booksQueryResult.data;
+  let {allBooks: books} = booksQueryResult.data;
+
+  const genres = [...new Set(books.map(book => book.genres).flat())].sort();
+  if (genreFilter !== null) {
+    books = books.filter(book => book.genres.includes(genreFilter));
+  }
 
   return (
     <div>
@@ -20,22 +27,39 @@ const Books = (props) => {
 
       <table>
         <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
+        <tr>
+          <th></th>
+          <th>author</th>
             <th>published</th>
+        </tr>
+        {books.map((a) => (
+          <tr key={a.title}>
+            <td>{a.title}</td>
+            <td>{a.author.name}</td>
+            <td>{a.published}</td>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+        ))}
         </tbody>
       </table>
+
+      <div>
+        {genres.map(genre => (
+          <button
+            key={genre}
+            type={'button'}
+            onClick={() => setGenreFilter(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button
+          type={'button'}
+          onClick={() => setGenreFilter(null)}
+          >
+          all genres
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Books
