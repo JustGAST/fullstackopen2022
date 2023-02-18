641a0fc3959ab7e2dc34ee0192c560dcb951ec28
justgast <justgast@gmail.com>
2023-02-18 12:29:37 +0300

    Part 9: Exercise 9.3 ready;

--- part9/typescript_first_steps/bmiCalculator.ts
+++ part9/typescript_first_steps/bmiCalculator.ts
@@ -20,4 +20,26 @@ const calculateBmi = (weight: number, height: number): string => {
     }
 }
 
-console.log(calculateBmi(55, 1.70))
+interface BmiParams {
+    weight: number,
+    height: number,
+}
+const parseArgs = (args: string[]): BmiParams => {
+    if (args.length > 4) {
+        throw new Error("Too many params")
+    }
+    if (args.length < 4) {
+        throw new Error("Too few params")
+    }
+
+    const weight = Number(args[3]);
+    const height = Number(args[2]);
+    if (isNaN(weight) || isNaN(height)) {
+        throw new Error("Params should be numbers")
+    }
+
+    return {weight, height};
+}
+
+const args = parseArgs(process.argv)
+console.log(calculateBmi(args.weight, args.height))
--- part9/typescript_first_steps/exerciseCalculator.ts
+++ part9/typescript_first_steps/exerciseCalculator.ts
@@ -39,4 +39,25 @@ const calculateExercises = (hours: number[], target: number): Result => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const parseExerciseArgs = (args: string[]): { hours: number[], target: number } =>  {
+    if (args.length < 3) {
+        throw new Error("Provide exercise hours by days")
+    }
+
+    const argsNumbers: number[] = args.slice(2).map(arg => {
+        const argNumber = Number(arg)
+        if (isNaN(argNumber)) {
+            throw new Error("Arguments should be numbers");
+        }
+        if (argNumber > 24) {
+            throw new Error("Specify hours of day, not more")
+        }
+
+        return argNumber;
+    })
+
+    return {hours: argsNumbers.slice(1), target: argsNumbers[0]}
+}
+
+const exerciseArgs = parseExerciseArgs(process.argv)
+console.log(calculateExercises(exerciseArgs.hours, exerciseArgs.target))
