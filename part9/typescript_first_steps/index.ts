53ee399c640de05c95708311d40a1530eb7803b3
justgast <justgast@gmail.com>
2023-02-18 13:02:20 +0300

    Part 9: Exercise 9.5 ready;

--- part9/typescript_first_steps/index.ts
+++ part9/typescript_first_steps/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import {calculateBmi} from "./calculateBmi";
+
 const app = express()
 
 app.get('/ping', (_req, res) => {
@@ -8,6 +10,33 @@ app.get('/ping', (_req, res) => {
 
 app.get('/hello', (_req, res) => {
     res.send("Hello Full Stack!")
+})
+
+app.get('/bmi', (req, res) => {
+    if (!(req.query.hasOwnProperty('weight') && req.query.hasOwnProperty('height'))) {
+        res.status(400).json({
+            error: "Specify weight and height parameters"
+        })
+        return;
+    }
+
+    let {weight, height} = req.query;
+
+    if (isNaN(Number(weight)) || isNaN(Number(height))) {
+        res.status(400).json({
+            error: "Weight and height should be numbers"
+        })
+        return;
+    }
+
+    const validatedWeight = Number(weight)
+    const validatedHeight = Number(height)
+
+    const bmi = calculateBmi(validatedWeight, validatedHeight);
+
+    res.json({
+        result: bmi
+    });
 })
 
 const PORT = 3003;
