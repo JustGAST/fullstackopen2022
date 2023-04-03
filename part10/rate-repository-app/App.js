4ba86d7172abaa81e65ba3802f3846ce435211a2
justgast <justgast@gmail.com>
2023-04-03 16:44:56 +0300

    Part 10: Exercise 10.12 ready;

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,13 +1,16 @@
 import {NativeRouter} from 'react-router-native';
 import {StatusBar} from 'expo-status-bar';
+import Constants from 'expo-constants';
+import {ApolloProvider} from '@apollo/client';
 
 import Main from './src/components/Main';
-import {ApolloProvider} from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
 
 const apolloClient = createApolloClient();
 
 export default function App() {
+  console.log(JSON.stringify(Constants.manifest.extra, null, 2));
+
   return (
     <>
       <NativeRouter>
