562adfed406fd9cb37c345be36bae2ad82f76a0d
justgast <justgast@gmail.com>
2023-04-18 14:19:43 +0300

    Part 10: Exercise 10.18 ready;

--- part10/rate-repository-app/src/__tests__/components/SignInForm.test.js
+++ part10/rate-repository-app/src/__tests__/components/SignInForm.test.js
@@ -1,13 +1,11 @@
-import {fireEvent, render, screen, act} from '@testing-library/react-native';
+import {fireEvent, render, screen, act, waitFor} from '@testing-library/react-native';
 
 import SignInForm from '../../components/SignInForm';
 
 describe('SignInForm', () => {
-  it('calls onsubmit function', () => {
+  it('calls onsubmit function', async () => {
     const onSubmitMock = jest.fn();
-    act(() => {
-      render(<SignInForm onSubmit={onSubmitMock}/>);
-    })
+    render(<SignInForm onSubmit={onSubmitMock}/>);
 
     fireEvent.changeText(screen.getByPlaceholderText('Login'), 'matti');
     fireEvent.changeText(screen.getByPlaceholderText('Password'), 'password');
@@ -15,10 +13,12 @@ describe('SignInForm', () => {
       fireEvent.press(screen.getByText('Login'));
     })
 
-    expect(onSubmitMock).toHaveBeenCalledTimes(1);
-    expect(onSubmitMock.mock.calls[0][0]).toEqual({
-      login: 'matti',
-      password: 'password'
+    await waitFor(() => {
+      expect(onSubmitMock).toHaveBeenCalledTimes(1);
+      expect(onSubmitMock.mock.calls[0][0]).toEqual({
+        username: 'matti',
+        password: 'password'
+      })
     })
   })
 })
