2d015af7ebea9349c6e8fff221271fda78b256c7
justgast <justgast@gmail.com>
2023-04-26 20:47:21 +0300

    Part 10: Exercise 10.20 in-progress;

--- part10/rate-repository-app/src/components/ReviewItem.js
+++ part10/rate-repository-app/src/components/ReviewItem.js
@@ -1,13 +1,67 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Text from './Text.js';
+import theme from '../theme.js';
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 5,
+    backgroundColor: '#cecece',
+    marginHorizontal: 10,
+    padding: 10,
+  },
+  item: {
+    flexDirection: "row"
+  },
+  rating: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  avatarColumn: {
+    width: 60,
+  },
+  infoColumn: {
+    maxWidth: 310,
+  },
+  description: {
+    marginVertical: 10,
+  },
+  languageContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start"
+  },
+  language: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    padding: 5,
+  },
+  perks: {
+    flexDirection: "row",
+    marginTop: 10
+  },
+  button: {
+    marginTop: 10
+  }
+});
+
 
 const ReviewItem = ({ review }) => {
   return (
-    <View key={review.id}>
-      <Text>Rating: {review.rating}</Text>
-      <Text>Review: {review.text}</Text>
-      <Text>Created at: {review.createdAt}</Text>
-      <Text>Username: {review.user.username}</Text>
+    <View key={review.id} style={[styles.container, styles.item]}>
+      <View style={styles.avatarColumn}>
+        <View style={styles.rating}>
+          <Text>{review.rating}</Text>
+        </View>
+      </View>
+      <View style={styles.infoColumn}>
+        <Text>Review: {review.text}</Text>
+        <Text>Created at: {review.createdAt}</Text>
+        <Text>Username: {review.user.username}</Text>
+      </View>
     </View>
   )
 }
