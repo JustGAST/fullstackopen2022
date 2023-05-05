cda35222145bd1011f9ff4165901c582a46311de
justgast <justgast@gmail.com>
2023-05-05 18:00:03 +0300

    Part 10: Exercise 10.21 in-progress;

---
+++ part10/rate-repository-app/src/hooks/useCreateReview.js
@@ -0,0 +1,28 @@
+import {useMutation} from '@apollo/client';
+import {CREATE_REVIEW} from '../graphql/mutations.js';
+
+
+const useCreateReview = () => {
+  const [mutate, result] = useMutation(CREATE_REVIEW);
+
+  const createReview = async ({repositoryName, ownerName, rating, text}) => {
+    const result = await mutate({
+      variables: {repositoryName, ownerName, rating, text}
+    });
+
+    if (!(
+      result
+      && result.hasOwnProperty('data')
+      && result.data.hasOwnProperty('createReview')
+    )
+    ) {
+      return null;
+    }
+
+    return result.data.createReview;
+  }
+
+  return [createReview, result];
+}
+
+export default useCreateReview;
