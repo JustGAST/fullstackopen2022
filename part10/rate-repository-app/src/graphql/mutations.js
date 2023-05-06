60d2aa11b959b07d040e0b455dca450427d9eca0
justgast <justgast@gmail.com>
2023-05-06 11:23:46 +0300

    Part 10: Exercise 10.21 in-progress;

--- part10/rate-repository-app/src/graphql/mutations.js
+++ part10/rate-repository-app/src/graphql/mutations.js
@@ -12,3 +12,11 @@ export const AUTHENTICATE = gql`
       }
   }
 `;
+
+export const CREATE_REVIEW = gql`
+  mutation ($repositoryName: String!, $ownerName: String!, $rating: Int!, $text: String) {
+      createReview (review: {repositoryName: $repositoryName, ownerName: $ownerName, rating: $rating, text: $text}) {
+          repositoryId
+      }
+  }
+`;
